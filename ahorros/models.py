d54eb4365a6c5d414515dcc3612f6094c4d67d6e
valeriavcgr <valeria3025gon@gmail.com>
2026-03-23 10:46:42 -0500

    [FIX] back modulo de ahorros terminado

--- ahorros/models.py
+++ ahorros/models.py
@@ -20,11 +20,11 @@ class AhorroMeta(models.Model):
         ABANDONADO='ABANDONADO', 'Abandonado',
         
     monto_meta=models.DecimalField(max_digits=12, decimal_places=2, null=False)
-    total_acumulado=models.DecimalField(max_digits=12, decimal_places=2, null=False)
+    total_acumulado=models.DecimalField(max_digits=12, decimal_places=2, null=False, default=0)
     frecuencia=models.CharField(max_length=15, choices=Frecuencia.choices, null=False)
     fecha_creacion = models.DateField(auto_now_add=True, null=False)
     fecha_meta=models.DateField(null=False)
-    estado= models.CharField(max_length=25, choices=Estado.choices, null=False)
+    estado= models.CharField(max_length=25, choices=Estado.choices, null=False, default=Estado.SIN_INICIAR)
     cantidad_cuotas=models.IntegerField(null=False)
     descripcion=models.CharField(max_length=150, blank=True)
     categoria = models.ForeignKey("categorias.categoria", on_delete= models.CASCADE, null=False)
@@ -47,11 +47,12 @@ class AporteAhorro(models.Model):
         PENDIENTE='PENDIENTE', 'Pendiente'
         
     aporte_asignado=models.DecimalField(max_digits=12, decimal_places=2 )
-    aporte=models.DecimalField(max_digits=12, decimal_places=2)
+    aporte = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
     fecha_limite=models.DateField(null=False)
     estado_ap=models.CharField(max_length=15, choices=EstadoAp.choices, null=False)
     fecha_registro = models.DateField(auto_now_add=True, null=False)
     ahorro=models.ForeignKey(AhorroMeta, on_delete=models.CASCADE, null=False)
+    
     
     
     class Meta:
